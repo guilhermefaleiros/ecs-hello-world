e776240b9c53151a8f55ffe5fd360203103a327b
Guilherme Faleiros <guilhermefaleiros2000@gmail.com>
2024-12-26 20:53:01 -0300

    fix: variable interpolation

--- terraform/main.tf
+++ terraform/main.tf
@@ -167,7 +167,7 @@ resource "aws_ecs_service" "ecs_service" {
 
   load_balancer {
     target_group_arn = aws_lb_target_group.target_group.arn
-    container_name   = "${var.project_name}"
+    container_name   = var.project_name
     container_port   = var.container_port
   }
 
