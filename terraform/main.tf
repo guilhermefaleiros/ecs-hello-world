9aaf6804250e3c44f7c8de08572ff21f3892551e
Guilherme Faleiros <guilhermefaleiros2000@gmail.com>
2024-12-27 07:45:25 -0300

    fix: adding deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,110 +66,110 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
   ])
 }
 
-# # VPC
-# module "vpc" {
-#   source = "terraform-aws-modules/vpc/aws"
+# VPC
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-#   name             = "${var.project_name}-vpc"
-#   cidr             = var.vpc_cidr
-#   azs              = var.availability_zones
-#   private_subnets  = var.private_subnets
-#   public_subnets   = var.public_subnets
-#   enable_nat_gateway = true
-# }
+  name             = "${var.project_name}-vpc"
+  cidr             = var.vpc_cidr
+  azs              = var.availability_zones
+  private_subnets  = var.private_subnets
+  public_subnets   = var.public_subnets
+  enable_nat_gateway = true
+}
 
-# # Security Groups
-# resource "aws_security_group" "alb_sg" {
-#   name        = "${var.project_name}-alb-sg"
-#   description = "Allow HTTP traffic"
-#   vpc_id      = module.vpc.vpc_id
+# Security Groups
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.project_name}-alb-sg"
+  description = "Allow HTTP traffic"
+  vpc_id      = module.vpc.vpc_id
 
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-# resource "aws_security_group" "task_sg" {
-#   name        = "${var.project_name}-task-sg"
-#   description = "Allow ALB to communicate with ECS tasks"
-#   vpc_id      = module.vpc.vpc_id
+resource "aws_security_group" "task_sg" {
+  name        = "${var.project_name}-task-sg"
+  description = "Allow ALB to communicate with ECS tasks"
+  vpc_id      = module.vpc.vpc_id
 
-#   ingress {
-#     from_port       = var.container_port
-#     to_port         = var.container_port
-#     protocol        = "tcp"
-#     security_groups = [aws_security_group.alb_sg.id]
-#   }
+  ingress {
+    from_port       = var.container_port
+    to_port         = var.container_port
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-# # ALB
-# resource "aws_lb" "alb" {
-#   name               = "${var.project_name}-alb"
-#   internal           = false
-#   load_balancer_type = "application"
-#   security_groups    = [aws_security_group.alb_sg.id]
-#   subnets            = module.vpc.public_subnets
-# }
+# ALB
+resource "aws_lb" "alb" {
+  name               = "${var.project_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = module.vpc.public_subnets
+}
 
-# resource "aws_lb_target_group" "target_group" {
-#   name        = "${var.project_name}-tg"
-#   port        = var.container_port
-#   protocol    = "HTTP"
-#   vpc_id      = module.vpc.vpc_id
-#   target_type = "ip"
-# }
+resource "aws_lb_target_group" "target_group" {
+  name        = "${var.project_name}-tg"
+  port        = var.container_port
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
+  target_type = "ip"
+}
 
-# resource "aws_lb_listener" "alb_listener" {
-#   load_balancer_arn = aws_lb.alb.arn
-#   port              = 80
-#   protocol          = "HTTP"
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.target_group.arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
 
-# # ECS Cluster
-# resource "aws_ecs_cluster" "ecs_cluster" {
-#   name = "${var.project_name}-cluster"
-# }
+# ECS Cluster
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "${var.project_name}-cluster"
+}
 
-# # ECS Service
-# resource "aws_ecs_service" "ecs_service" {
-#   name            = "${var.project_name}-service"
-#   cluster         = aws_ecs_cluster.ecs_cluster.id
-#   task_definition = aws_ecs_task_definition.ecs_task_definition.arn
-#   launch_type     = "FARGATE"
+# ECS Service
+resource "aws_ecs_service" "ecs_service" {
+  name            = "${var.project_name}-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.ecs_task_definition.arn
+  launch_type     = "FARGATE"
 
-#   network_configuration {
-#     subnets         = module.vpc.private_subnets
-#     security_groups = [aws_security_group.task_sg.id]
-#     assign_public_ip = false
-#   }
+  network_configuration {
+    subnets         = module.vpc.private_subnets
+    security_groups = [aws_security_group.task_sg.id]
+    assign_public_ip = false
+  }
 
-#   load_balancer {
-#     target_group_arn = aws_lb_target_group.target_group.arn
-#     container_name   = var.project_name
-#     container_port   = var.container_port
-#   }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn
+    container_name   = var.project_name
+    container_port   = var.container_port
+  }
 
-#   desired_count = 1
-# }
+  desired_count = 1
+}
